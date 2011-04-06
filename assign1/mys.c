baf2091ad50038a1ae464ae1d63a4e18f5d88f35
Mathew Duhon <mduhon@235x08.csc.calpoly.edu>
2011-04-06 15:54:55 -0700

    got more done on mys

--- assign1/mys.c
+++ assign1/mys.c
@@ -6,6 +6,9 @@
 #include<unistd.h>
 #include<sys/types.h>
 #include<sys/ioctl.h>
+#include<sys/wait.h>
+
+#define BUF_SIZE 100
 
 
 static void get_winsize(int fd, struct winsize *ws); 
@@ -15,11 +18,14 @@ void raw_mode(int fd, struct termios *old);
 
 
 int main (int argc,char * argv[]) {
-    pid_t id;
-    int outfile;
+    pid_t id, input, slave;
+    int outfile, pty;
+    char inData [BUF_SIZE];
+    char outData [BUF_SIZE];
     struct winsize win;
     struct termios original;
     
+    /*Opens the correct output file depending on the request */
     if(argc != 1 && argc != 2) {
         perror("Please enter correct # of arguments\n");
         exit(-1);
@@ -29,16 +35,61 @@ int main (int argc,char * argv[]) {
     } else {
         outfile = open("typescript",O_RDWR, O_CREAT | O_TRUNC); 
     }
+
+    /*Print pid incase I need to terminate it manually */
     id = getpid();
     fprintf(stderr, "Parent process id is %d\n",id);
 
-    if(isatty(0) != 1){
+    /*make sure you are in a tty */
+    if(isatty(STDIN_FILENO) != 1){
         perror("Not a tty\n");
         exit(-1);
     }
-    get_winsize(0,&win);
-    raw_mode(0, &original);
+    /*get window size for later and then palce into raw mode*/
+    get_winsize(STDIN_FILENO,&win);
+    raw_mode(STDIN_FILENO, &original);
 
+    /*opens the pty and sets flags */
+    if((pty = posix_openpt(O_RDWR | O_NOCTTY)) == -1) {
+        perror("No pty");
+        exit(-1);
+    }
+    
+    /*do slave stuff */
+    if(!(slave = fork())){
+
+        exit(1);
+    }
+    /*do input stuff */
+    if(!(input = fork())){
+        /*set up sig handler*/
+        
+        while(read(STDIN_FILENO, inData, BUF_SIZE) > 0) {
+            write(pty, inData, BUF_SIZE);
+        }
+        close(pty);
+        close(outfile);
+        exit(1);
+    }
+    /*read from pty and write to both log and stdout*/
+
+    /*send signal to input*/
+
+
+    if ( -1 == wait(NULL) ) { /* wait for slave */
+
+        perror("wait");
+    }
+    if ( -1 == wait(NULL) ) { /* wait for input */
+        /*set up sig handler*/
+
+        perror("wait");
+    }
+
+    /* clean up stuff */
+    restore_mode(STDIN_FILENO, &original);
+    set_winsize(STDIN_FILENO,&win);
+    close(outfile);
 
     return 0;
 }
